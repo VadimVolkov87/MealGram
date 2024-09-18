6725b2bf7af6b335eba022cc39cfc0f65e3bfbe6
Vadim Volkov <bcdy2146@yandex.ru>
2024-09-18 21:30:57 +0300

    Project depah

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Secretkey')
 
 DEBUG = os.getenv('DEBUG', 'False')
 
-ALLOWED_HOSTS = str.split(os.getenv('ALLOWED_HOSTS', ''))
+ALLOWED_HOSTS = str.split(os.getenv(
+    'ALLOWED_HOSTS', 'dipfoodgram24.zapto.org'))
 
 
 INSTALLED_APPS = [
@@ -122,7 +123,6 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
