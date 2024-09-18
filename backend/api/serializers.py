dd9618f71a3c44a35fd64f38b463584b02280229
Vadim Volkov <bcdy2146@yandex.ru>
2024-09-18 16:32:24 +0300

    Project depwt2

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -36,7 +36,7 @@ class FoodgramUserSerializer(UserSerializer):
 
     def validate(self, data):
         """Метод валидации количества."""
-        if data == {}:  # Та же история.
+        if data == {}:  # Та же история, не ловится без метода.
             raise serializers.ValidationError(
                 'Поле аватара не может быть пустым.',
                 code=400
